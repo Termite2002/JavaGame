ddbdff2813e4a87d73991c2bcb486cbfdbaf910f
Termite2002 <motsach1732002@gmail.com>
2022-09-01 16:42:13 +0700

    4

--- Javagame/src/main/GamePanel.java
+++ Javagame/src/main/GamePanel.java
@@ -3,27 +3,42 @@ package main;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
+
+import entity.Player;
 
 public class GamePanel extends JPanel implements Runnable {						// kế thừa class JPanel
 	// SCREEN SETTINGS
     final int originalTitleSize = 16;											// 16 x 16
     final int scale = 3;
     
-    final int titleSize = originalTitleSize * scale;							// 48 x 48
+    public final int titleSize = originalTitleSize * scale;							// 48 x 48
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
-    final int screenWidth = titleSize * maxScreenCol;
-    final int screenHeight = titleSize * maxScreenRow;
+    final int screenWidth = titleSize * maxScreenCol; 							// chiều ngang cửa sổ window
+    final int screenHeight = titleSize * maxScreenRow;							// chiều dọc cửa sổ window
     
+    // FPS
+    int FPS = 60;
+    
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-
+    Player player = new Player(this, keyH);
+    
+    // Set tam pos char
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+    
+    
     public GamePanel() {
-        this.setPreferredSize(new Dimension(screenWidth, screenHeight));		// thiet lap kich thuoc cho class JPanel
-        this.setBackground(Color.black);										// mau nen
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));		// thiết lập kích thước cho class JPanel
+        this.setBackground(Color.black);										// tạo màu nền
         this.setDoubleBuffered(true);											// Để true, mọi hoạt ảnh của thành phần này đều được hoàn thành trong bộ đệm chạy ngầm
-        
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
     
     public void startGameThread() {
@@ -33,24 +48,45 @@ public class GamePanel extends JPanel implements Runnable {						// kế thừa 
 
 	@Override
 	public void run() {
+		
+		double drawInterval = 1000000000/FPS;
+		double nextDrawTime = System.nanoTime() + drawInterval;
+		
 		// Core: Game loop
 		while(gameThread != null) {
+			
+			
 			// 1 UPDATE: Update information in game
 			update();
 			
 			// 2 DRAW: Draw updated information on screen
 			repaint();
 			
+			
+			try {
+				double remainingTime = nextDrawTime - System.nanoTime();
+				remainingTime /= 1000000;
+				if(remainingTime < 0) {
+					remainingTime = 0;
+				}
+				
+				Thread.sleep((long) remainingTime);
+				nextDrawTime = System.nanoTime() + drawInterval;
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	public void update() {
-		
+		player.update();
 	}
 	public void paintComponent(Graphics g) {									// paintComponent là 1 hàm dựng sẵn trong java dùng để vẽ trên JPanel truyền vào lớp Graphics(1 lớp có nhiều hàm vẽ)
 		super.paintComponent(g);												// super dùng để gọi method của class JPanel(superclass) mà GamePanel(subclass) kế thừa. paintComponent, phương thức này được override từ lớp JPanel
 		
-		
-		
+		Graphics2D g2 = (Graphics2D)g;
+		player.draw(g2);
+		g2.dispose();
 	}
 }
 
