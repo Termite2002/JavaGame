700225863d91d0ca4a0e856f39e5db15607c77af
Termite2002 <motsach1732002@gmail.com>
2022-09-02 08:27:32 +0700

    5

--- Javagame/src/entity/Player.java
+++ Javagame/src/entity/Player.java
@@ -2,6 +2,7 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -44,21 +45,70 @@ public class Player extends Entity{
 	}
 	public void update() {
 		if(keyH.upPressed == true) {
+			direction = "up";
 			y -= speed;
 		}
 		else if(keyH.downPressed == true) {
+			direction = "down";
 			y += speed;
 		}
 		else if(keyH.leftPressed == true) {
+			direction = "left";
 			x -= speed;
 		}
 		else if(keyH.rightPressed == true) {
+			direction = "right";
 			x += speed;
+		}
+		
+		spriteCounter++;
+		if(spriteCounter > 15) {
+			if (spriteNum == 1) spriteNum = 2;
+			else if (spriteNum == 2) spriteNum = 1;
+			spriteCounter = 0;
 		}
 	}
 	public void draw(Graphics2D g2) {
-		g2.setColor(Color.white);
-		g2.fillRect(x, y, gp.titleSize, gp.titleSize);
+//		g2.setColor(Color.white);
+//		g2.fillRect(x, y, gp.titleSize, gp.titleSize);
 		
+		BufferedImage image = null;
+		
+		switch(direction) {
+		case "up":
+			if(spriteNum == 1) {
+				image = up1;
+			}
+			if(spriteNum == 2) {
+				image = up2;
+			}
+			break;
+		case "down":
+			if(spriteNum == 1) {
+				image = down1;
+			}
+			if(spriteNum == 2) {
+				image = down2;
+			}
+			break;
+		case "left":
+			if(spriteNum == 1) {
+				image = left1;
+			}
+			if(spriteNum == 2) {
+				image = left2;
+			}
+			break;
+		case "right":
+			if(spriteNum == 1) {
+				image = right1;
+			}
+			if(spriteNum == 2) {
+				image = right2;
+			}
+			break;
+		}
+		g2.drawImage(image, x, y, gp.titleSize, gp.titleSize, null);
 	}
+	
 }
